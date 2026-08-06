d93e53903e5db41a054330bb45b76ee49ab5ffc8
foobarkok <008900014051nnvm@gl.pen-kanagawa.ed.jp>
2026-08-06 15:49:06 +0900

    fix write_color

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,9 @@
 use crate::vec3::Vec3;
 
 pub fn write_color(color: Vec3) {
-    let r = (color.x * 259.99) as i32;
-    let g = (color.y * 259.99) as i32;
-    let b = (color.z * 259.99) as i32;
+    let r = (color.x * 255.99) as i32;
+    let g = (color.y * 255.99) as i32;
+    let b = (color.z * 255.99) as i32;
 
     println!("{r} {g} {b}");
 }
