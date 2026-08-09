3dad26f854055e4c2cb4d9a6271d67815dbabb60
foobarkok <008900014051nnvm@gl.pen-kanagawa.ed.jp>
2026-08-09 22:21:18 +0900

    improve write_color

--- src/color.rs
+++ src/color.rs
@@ -1,11 +1,9 @@
-use crate::interval::Interval;
 use crate::vec3::Vec3;
 
 pub fn write_color(color: Vec3) {
-    const INTENSITY: Interval = Interval::new(0.0, 0.999);
-    let r = (256.0 * INTENSITY.clamp(color.x)) as i32;
-    let g = (256.0 * INTENSITY.clamp(color.y)) as i32;
-    let b = (256.0 * INTENSITY.clamp(color.z)) as i32;
+    let r = (256.0 * color.x.clamp(0.0, 0.999)) as i32;
+    let g = (256.0 * color.y.clamp(0.0, 0.999)) as i32;
+    let b = (256.0 * color.z.clamp(0.0, 0.999)) as i32;
 
     println!("{r} {g} {b}");
 }
