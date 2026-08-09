58539f2e3ef172b55729e5f35d3c55c2081ed912
foobarkok <008900014051nnvm@gl.pen-kanagawa.ed.jp>
2026-08-09 22:59:29 +0900

    gamma-correct

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,20 @@
 use crate::vec3::Vec3;
 
+fn liner2gamma(liner: f64) -> f64 {
+    if liner > 0.0 {
+        return liner.sqrt();
+    }
+    0.0
+}
+
 pub fn write_color(color: Vec3) {
-    let r = (256.0 * color.x.clamp(0.0, 0.999)) as i32;
-    let g = (256.0 * color.y.clamp(0.0, 0.999)) as i32;
-    let b = (256.0 * color.z.clamp(0.0, 0.999)) as i32;
+    let r = liner2gamma(color.x);
+    let g = liner2gamma(color.y);
+    let b = liner2gamma(color.z);
+
+    let r = (256.0 * r.clamp(0.0, 0.999)) as i32;
+    let g = (256.0 * g.clamp(0.0, 0.999)) as i32;
+    let b = (256.0 * b.clamp(0.0, 0.999)) as i32;
 
     println!("{r} {g} {b}");
 }
