d08692483ae667901be47d633a9c8912d583eb47
foobarkok <008900014051nnvm@gl.pen-kanagawa.ed.jp>
2026-08-12 14:24:47 +0900

    groupのぶん機を修正

--- src/main.rs
+++ src/main.rs
@@ -159,9 +159,11 @@ fn render_with_group() {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() > 0 && args[0] == "default" {
+    if args.len() > 1 && args[1] == "default" {
+        eprintln!("default mode");
         render_default();
     } else {
+        eprintln!("group mode");
         render_with_group();
     }
 }
